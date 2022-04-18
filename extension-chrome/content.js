521ef8d3e122b9a58c4e1b766acedbe73cfccf20
Briolant Quentin <quentin.briolant@epita.fr>
2022-04-18 17:05:17 +0200

    feat (extension): add options & switcher

--- extension-chrome/content.js
+++ extension-chrome/content.js
@@ -33,10 +33,28 @@ function init() {
   <div class="titlebox-title card-header">
     <span class="toggle " data-toggle="collapse" data-target="#netiquette-checker" title="Toggle visibility"></span>
     <span class="left">Netiquette Checker - Infinity RT</span>
-    <span class="right-empty"></span>
+    <span class="right" id="netiquette-switcher"><svg class="svg-inline--fa fa-cog fa-w-16 icon-bordered fa-2x" alt="Options" data-toggle="tooltip" data-placement="top" data-original-title="Options" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="cog" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M487.4 315.7l-42.6-24.6c4.3-23.2 4.3-47 0-70.2l42.6-24.6c4.9-2.8 7.1-8.6 5.5-14-11.1-35.6-30-67.8-54.7-94.6-3.8-4.1-10-5.1-14.8-2.3L380.8 110c-17.9-15.4-38.5-27.3-60.8-35.1V25.8c0-5.6-3.9-10.5-9.4-11.7-36.7-8.2-74.3-7.8-109.2 0-5.5 1.2-9.4 6.1-9.4 11.7V75c-22.2 7.9-42.8 19.8-60.8 35.1L88.7 85.5c-4.9-2.8-11-1.9-14.8 2.3-24.7 26.7-43.6 58.9-54.7 94.6-1.7 5.4.6 11.2 5.5 14L67.3 221c-4.3 23.2-4.3 47 0 70.2l-42.6 24.6c-4.9 2.8-7.1 8.6-5.5 14 11.1 35.6 30 67.8 54.7 94.6 3.8 4.1 10 5.1 14.8 2.3l42.6-24.6c17.9 15.4 38.5 27.3 60.8 35.1v49.2c0 5.6 3.9 10.5 9.4 11.7 36.7 8.2 74.3 7.8 109.2 0 5.5-1.2 9.4-6.1 9.4-11.7v-49.2c22.2-7.9 42.8-19.8 60.8-35.1l42.6 24.6c4.9 2.8 11 1.9 14.8-2.3 24.7-26.7 43.6-58.9 54.7-94.6 1.5-5.5-.7-11.3-5.6-14.1zM256 336c-44.1 0-80-35.9-80-80s35.9-80 80-80 80 35.9 80 80-35.9 80-80 80z"></path></svg></span>
   </div>
   <div class="titlebox-content collapse show " id="netiquette-checker">
     <div class="card-body" id="netiquette-body">
+    </div>
+    
+    <div class="card-body" id="netiquette-options" style="display: none;">
+        <div class="custom-control custom-checkbox">
+            <input type="checkbox" class="custom-control-input" name="netiquette-disable-submit" id="netiquette-disable-submit">
+            <label class="custom-control-label" for="netiquette-disable-submit">
+                Disabled submit if netiquette is not respected
+            </label>
+        </div>
+        
+        <div class="custom-control custom-checkbox">
+            <input type="checkbox" class="custom-control-input" name="netiquette-active-signature" id="netiquette-active-signature">
+            <label class="custom-control-label" for="netiquette-active-signature">
+                Enable signature
+            </label>
+        </div>
+        
+        <textarea autocomplete="off" class="form-control messagebox " wrap="soft" id="netiquette-signature" placeholder="Signature" rows="4"></textarea>
     </div>
   </div>
 </div>`;
@@ -46,6 +64,8 @@ function init() {
     /** @type {HTMLTextAreaElement} */
     const textInput = document.querySelector('#UpdateContent');
     const netiquetteBody = document.querySelector('#netiquette-body');
+    const netiquetteSwitcher = document.querySelector('#netiquette-switcher');
+    const netiquetteOptions = document.querySelector('#netiquette-options');
     const headerBlock = netiquetteBody.parentElement.parentElement;
 
     if (!textInput) return;
@@ -53,7 +73,32 @@ function init() {
     textInput.addEventListener('input', () => checkNetiquette(textInput, headerBlock, netiquetteBody));
     checkNetiquette(textInput, headerBlock, netiquetteBody);
 
-    textInput.textContent += `\n-- \nQuentin Briolant <quentin.briolant@epita.fr>\nIng2 - 2023\nYAKA - LAB SI - 2023`
+    netiquetteSwitcher.addEventListener('click', () => switchNetiquetteView(netiquetteBody, netiquetteOptions));
+}
+
+/**
+ * @param {HTMLDivElement} netiquetteBody
+ * @param {HTMLDivElement} netiquetteOptions
+ */
+function switchNetiquetteView(netiquetteBody, netiquetteOptions) {
+    const oldElement = netiquetteBody.style.display === 'none' ? netiquetteOptions : netiquetteBody;
+    const newElement = netiquetteBody.style.display === 'none' ? netiquetteBody : netiquetteOptions;
+
+    newElement.style.display = 'block';
+    const oldHeight = oldElement.scrollHeight + 'px';
+    const newHeight = newElement.scrollHeight + 'px';
+    oldElement.style.display = 'none';
+    newElement.style.height = oldHeight;
+    newElement.style.animation = 'none';
+    newElement.animate({
+            height: newHeight
+        }, {
+            duration: 150,
+        }
+    );
+    setTimeout(() => {
+        newElement.style.height = '';
+    }, 150);
 }
 
 /**
